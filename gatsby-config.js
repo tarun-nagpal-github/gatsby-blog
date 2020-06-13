81481164feccd08ba2eddf0335a5e7df2614d0af
Vishal Garg <vishal.g@appwrk.com>
2020-06-13 12:46:46 +0530

    Added the GA to blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,12 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options : {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none"
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
